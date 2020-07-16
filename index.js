51190ec721486668b186cf498a23314eb7859904
Lekmiti Wafaa <lekmiti.wafaa@gmail.com>
2020-07-16 21:43:03 +0200

    use div based grid instead of table + initialize grid with two number at startup

--- index.js
+++ index.js
@@ -1,14 +1,53 @@
 let gridSize = 4;
+let grid;
+const $grid = document.getElementById('grid');
+
 
 const generateGrid = () => {
-    const grid = document.querySelector('table');
     for (let x = 0; x< gridSize; x++){
-        const row = document.createElement("tr");
-        grid.append(row);
+        const $row = document.createElement("div");
+        $row.classList.add("grid-row");
+        $grid.append($row);
         for (let y = 0; y < gridSize; y++ ){
-            row.append(document.createElement("td"));
+            const $cell = document.createElement("div");
+            $cell.classList.add("grid-data");
+            $row.append($cell);
         }
     }
 }
 
-generateGrid();
+const initGame = () => {
+    grid = Array.from(Array(gridSize), () => new Array(gridSize))
+    let x1 = randomIntFromInterval(0, gridSize - 1);
+    let y1 = randomIntFromInterval(0, gridSize - 1);  
+    grid[x1][y1] = 2;
+
+    let x2 = randomIntFromInterval(0, gridSize - 1);
+    let y2 = randomIntFromInterval(0, gridSize - 1);
+
+    while(x1 === x2 && y1 === y2){
+         x2 = randomIntFromInterval(0, gridSize - 1);
+         y2 = randomIntFromInterval(0, gridSize - 1);
+    }
+    grid[x2][y2] = 2;  
+}
+
+const randomIntFromInterval = (min, max) => { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  const renderGridState = () => {
+      for (let i = 0; i < gridSize ; i++){
+        const $row = $grid.querySelectorAll(".grid-row")[i];
+        for (let j = 0; j < gridSize; j++){
+            const $cell = $row.querySelectorAll(".grid-data")[j];
+            $cell.innerHTML = grid[i][j] ?  grid[i][j] : '';
+        }
+      }
+  }
+
+
+
+  generateGrid();
+  initGame();
+  renderGridState();
