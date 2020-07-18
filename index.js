3048bf0c5e0da07764c919a25715882f883680cf
Lekmiti Wafaa <lekmiti.wafaa@gmail.com>
2020-07-18 07:47:16 +0200

    Handle keu up

--- index.js
+++ index.js
@@ -49,7 +49,17 @@ const initGame = () => {
          x2 = randomIntFromInterval(0, gridSize - 1);
          y2 = randomIntFromInterval(0, gridSize - 1);
     }
-    grid[x2][y2] = 2;  
+    grid[x2][y2] = 2;
+
+    grid[0][1] = 4;   
+    grid[1][1] = 4;  
+    grid[2][1] = 8;  
+    grid[3][1] = 8; 
+
+    grid[0][2] = 8;   
+    grid[1][2] = 4;  
+    grid[2][2] = 2;  
+    grid[3][2] = 2; 
 };
 
 const randomIntFromInterval = (min, max) => {  
@@ -80,8 +90,39 @@ document.addEventListener('keydown', (event) => {
 
 })   
 
+
+
+
 const moveUp = () => {
-    console.log("moveUp")
+
+    for (let j = 0; j < gridSize; j++){ // parcourir la grille verticallement
+        let f = 0;  // represente le plus petit indice dans la recherche du nombre similaire. Le f augmente des qu'une premiere operation est faite pour ne pas mélanger deux opérations
+        for (let i = 1; i < gridSize; i++){ // parcourir une colonne 
+            if (grid[i][j]) { 
+                for (let k = i - 1; k >= f ; k--){   // chercher le nombre similaire en haut de la meme colonne, en partant de la case juste au dessus d nombre courant
+                    if (grid[i][j] === grid[k][j]){  // Cas 01:  le cas ou on trouve un nombre similaire au dessus
+                        grid[k][j] =  grid[k][j]* 2; //          multiplier la valeur du nombre similaire trouvé
+                        f = k +1                     //          la valeur trouvé n'est plus concerné par la recherche. 
+                        grid[i][j] = undefined;      //          mettre la valeur du nombre courant à undefined vue qu'il a bougé
+                        break;                       //          on intrompt la boucle du 'k' car une décision a été prise pour le nombre courant (pour chaque valeur on fait une seul action à la fois)
+                   
+                    } else if (grid[k][j]){          // Cas 01: le cas ou on trouve en dessus un nombre non-similaire mais qui'est défini 
+                        grid[k+1][j] = grid[i][j]    //         remonte le nombre courant just en dessous du nombre non-similaire trouvé
+                        if(k+1 !== i){               //         traiter le cas ou le nombre courant ne monte pas (car le nombre non-similaire trouvé est juste en dessus), dans ce cas il faut pas mettre le mettre à undefined
+                            grid[i][j] = undefined;  //         si le nombre courant monte , le mettre à undefined  
+                        }
+                        break;                       //         on intrompt la boucle du 'k' car une décision a été prise pour le nombre courant (pour chaque valeur on fait une seul action à la fois)
+                    }
+                    if (k === f){                    // Cas 03: le cas ou nous avons parcourru toute les cases concernées en dessus mais aucun nombre (similaire ou pas) n'a été trouvé
+                        grid[k][j] = grid[i][j];     //         monter le nombre courant vert la derniere case concernée
+                        grid[i][j] = undefined;      //         mettre le nombre courant à undefined
+                    }
+                }
+            }
+            
+        }
+    }
+    renderGridState();
 }
 
 const moveDown = () => {
